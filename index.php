52c86a12a9e1ed72ec9ad8132eb8ebc630cb1f56
EdsonnOliveira <72781591+EdsonnOliveira@users.noreply.github.com>
2021-12-10 14:44:06 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,188 @@
+<html>
+    <head>
+        <title>Follow</title>
+        <?php include("PHP/meta.php");?>
+        <link rel="stylesheet" type="text/css" href="CSS/index.css">
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-65707772LJ"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-65707772LJ');
+        </script>
+    </head>
+    <body id='content'>
+        <?php include('PHP/header.php'); ?>
+        <section class='preview pvBig pvGradientPurple'>
+            <img class='imgParallax' src="IMG/Vetor/header_penpot.png" data-speed='5'>
+            <img class='imgParallax' src="IMG/Vetor/header_paperclip.png" data-speed='5'>
+            <img class='imgParallax' src="IMG/Vetor/header_papers.png" data-speed='5'>
+            <img class='imgParallax' src="IMG/Vetor/header_edding.png" data-speed='5'>
+            <img class='imgParallax' src="IMG/Vetor/header_flowerpot.png" data-speed='5'>
+            <img class='imgParallax' src="IMG/Vetor/header_pen.png" data-speed='5'>
+            <img class='imgParallax' src="IMG/Vetor/header_tablet.png" data-speed='5'>
+            <h1 class='txtCenter txtWhite txtShadow txtGarnet txtUpper'>uma agência</br> digital.</h1>
+            <a href="#body" id='arrow'><img src="IMG/Custom/arrow.png" alt=""></a>
+        </section>
+<!-- GetButton.io widget -->
+<script type="text/javascript">
+    (function () {
+        var options = {
+            whatsapp: "+5561981228475", // WhatsApp number
+            call_to_action: "Faça seu orçamento.", // Call to action
+            position: "right", // Position may be 'right' or 'left'
+            pre_filled_message: "Olá, quero fazer meu orçamento.", // WhatsApp pre-filled message
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+</script>
+<!-- /GetButton.io widget -->
+        <main id='body'>
+            <div class='divMain'>
+                <div class='divClassica' id='quemSomos'>
+                    <div>
+                        <div>
+                            <div>
+                                <img src="IMG/Person/hugo.png" alt="">
+                            </div>
+                            <h2 class='txtCenter txt600'>Hugo Bueno</h2>
+                            <h3 class='txtPurple txtItalic txtCenter txt500'>CEO</h3>
+                        </div>
+                        <div>
+                            <div>
+                                <img src="IMG/Person/edson.png" alt="">
+                            </div>
+                            <h2 class='txtCenter txt600'>Edson Pinheiro</h2>
+                            <h3 class='txtPurple txtItalic txtCenter txt500'>DEVELOPER</h3>
+                        </div>
+                    </div>
+                    <div>
+                        <span>
+                            <h1 class='txtBlack txtCenter'>Quem Somos</h1>
+                            <h3 class='txtBlack txtJustify txt400'>Somos especialistas no mercado de <b>Marketing Digital</b>. Através do nosso conhecimento, nossos profissionais atuam de maneira célere, prezando um serviço de qualidade no mais alto nível encontrado no Brasil.</h3>
+                            <div>
+                                <div style='border-right: 3px solid #7B28F2'>
+                                    <h1 class='txtCenter txtPurple txt500'>+175</h1>
+                                    <h4 class='txtCenter txtGrey txt400'>serviços</br>executados</h4>
+                                </div>
+                                <div>
+                                    <h1 class='txtCenter txtPurple txt500'>+60</h1>
+                                    <h4 class='txtCenter txtGrey txt400'>clientes</br>atendidos</h4>
+                                </div>
+                            </div>
+                        </span>
+                    </div>
+                </div>
+                <div class='divClassica' id='questoes'>
+                    <div>
+                        <div class='front'>
+                            <h1 class='txtPurpleGradient txtCenter txt800'>01</h1>
+                            <h2 class='txtBlack txtCenter txt600'>O que é</br>Marketing Digital?</h2>
+                        </div>
+                        <div class='back'>
+                            <h3 class='txtWhite txtCenter txt400'>O Marketing Digital é o marketing realizado em
+                                                                  ambiente digital. Ele usa ferramentas como redes sociais, e-mail marketing e sites.</h3>
+                        </div>
+                    </div>
+                    <div>
+                        <div class='front'>
+                            <h1 class='txtPurpleGradient txtCenter txt800'>02</h1>
+                            <h2 class='txtBlack txtCenter txt600'>Marketing Digital</br>na sua Empresa.</h2>
+                        </div>
+                        <div class='back'>
+                            <h3 class='txtWhite txtCenter txt400'>O ambiente web permite que as empresas se aproximem mais de seus públicos, e é nesse ponto que precisam saber se comunicar da forma correta.</h3>
+                        </div>
+                    </div>
+                    <div>
+                        <div class='front'>
+                            <h1 class='txtPurpleGradient txtCenter txt800'>03</h1>
+                            <h2 class='txtBlack txtCenter txt600'>Marketing Offline</h2>
+                        </div>
+                        <div class='back'>
+                            <h3 class='txtWhite txtCenter txt400'>O marketing offline é a divulgação fora dos meios online. Possui característica marcante de ser um meio de comunicação em massa.</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class='divClassica faixa'>
+                <h1 class='txtCenter txtWhite txt600 txtUpper'>Agregue <b class='txtPurple'>valor</b> a sua empresa, venda mais!</h1>
+            </div>
+            <div class='divMain'>
+                <div class='divClassica' id='servicos'>
+                    <h1 class='txtBlack txtCenter'>Nossos serviços</h1>
+                    <h2 class='txtBlack txtCenter txt400'>Focados em transformar vidas e negócios.</h2>
+                    <div>
+                        <div>
+                            <h1 class="icofont-cart-alt txtCenter"></h1>
+                            <h2 class='txtCenter txtPurple txt500'>WebSite</h2>
+                            <h3 class='txtBlack txtCenter txt400'>Tenha um site para sua empresa.</h3>
+                        </div>
+                        <div>
+                            <h1 class="icofont-picture txtCenter"></h1>
+                            <h2 class='txtCenter txtPurple txt500'>Artes</h2>
+                            <h3 class='txtBlack txtCenter txt400'>Tenha conteúdo de qualidade para suas redes sociais.</h3>
+                        </div>
+                        <div>
+                            <h1 class="icofont-picture txtCenter"></h1>
+                            <h2 class='txtCenter txtPurple txt500'>Social Media</h2>
+                            <h3 class='txtBlack txtCenter txt400'>Tenha uma equipe capacitada na gestão de suas redes sociais.</h3>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <h1 class="icofont-brand-appstore txtCenter"></h1>
+                            <h2 class='txtCenter txtPurple txt500'>Aplicações</h2>
+                            <h3 class='txtBlack txtCenter txt400'>Tenha seu próprio sistema, chega de pagar mensalidades!</h3>
+                        </div>
+                        <div>
+                            <h1 class="icofont-people txtCenter"></h1>
+                            <h2 class='txtCenter txtPurple txt500'>Consultoria</h2>
+                            <h3 class='txtBlack txtCenter txt400'>Aprenda com os resultados. As melhores estratégias do mercado.</h3>
+                        </div>
+                        <div>
+                            <h1 class="icofont-brand-apple txtCenter"></h1>
+                            <h2 class='txtCenter txtPurple txt500'>Logomarca</h2>
+                            <h3 class='txtBlack txtCenter txt400'>Crie sua identidade visual para seus clientes não se esquecerem de você.</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class='divClassica faixa'>
+                <h1 class='txtCenter txtWhite txt600 txtUpper'>a agência para o <b class='txtPurple'>seu</b> negócio.</h1>
+            </div>
+            <div class='divMain'>
+                <div class='divClassica' id='clientes'>
+                    <h1 class='txtBlack txtCenter'>Casos de Sucesso</h1>
+                    <h2 class='txtBlack txtCenter txt400'>Confira alguns de nossos clientes!</h2>
+                    <div>
+                        <div>
+                            <img src="IMG/Clientes/DoceSetembro.png" alt="">
+                        </div>
+                        <div>
+                            <img src="IMG/Clientes/NaCaixa.png" alt="">
+                        </div>
+                        <div>
+                            <img src="IMG/Clientes/Cupom.png" alt="">
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <img src="IMG/Clientes/grego.png" alt="">
+                        </div>
+                        <div>
+                            <img src="IMG/Clientes/SexLingerie.png" alt="">
+                        </div>
+                        <div>
+                            <img src="IMG/Clientes/manumania.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <?php include('PHP/footer.php'); ?>
+    </body>
+</html>
